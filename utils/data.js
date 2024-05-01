943e581241240d2a081c6f2ca20150b505b63c39
Gibin <gibinmgeorge2001@gmail.com>
2024-05-01 10:05:16 -0500

    added utils

---
+++ utils/data.js
@@ -0,0 +1,24 @@
+const usersData = [
+    {
+      username: "user1",
+      email: "user1@uu1.com"
+    },
+    {
+      username: "user2",
+      email: "user2@uu2.com"
+    }
+  ]
+  
+  const thoughtsData = [
+    {
+      thoughtText: "Text1",
+      username: "user1"
+    },
+    {
+      thoughtText: "Text2",
+      username: "user2"
+    }
+  ]
+  
+  
+  module.exports = { usersData, thoughtsData }
---
+++ utils/seed.js
@@ -0,0 +1,32 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { usersData, thoughtsData } = require('./data.js')
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('\n-------------- DATABASE SYNCED --------------n');
+
+  // Delete the collections if they exist
+  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (userCheck.length) {
+    await connection.dropCollection('users');
+  }
+  
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
+
+  const userSeed = await User.create(usersData);
+  const thoughtSeed = await Thought.create(thoughtsData);
+
+  console.log(userSeed);
+  console.log('\n-------------- USER DATA SEEDED --------------n');
+  console.log(thoughtSeed);
+  console.log('\n-------------- THOUGHT DATA SEEDED --------------n');
+
+  console.info('Seeding complete!!');
+  
+  process.exit(0);
+});
