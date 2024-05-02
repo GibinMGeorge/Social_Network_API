796b850b6a47d48e795d1f62e9a62100b5b7882d
Gibin <gibinmgeorge2001@gmail.com>
2024-05-02 14:36:51 -0500

    updated routes and initiated controllers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,64 +1,21 @@
 const router = require('express').Router();
-const { Thought } = require('../models');
+const {
+  getThoughts,
+  getSingleThought,
+  createThought
+} = require('../../controllers/thoughtController.js')
 
-// GET all thoughts
-router.get('/', async (req, res) => {
-  try {
-    const thoughts = await Thought.find();
-    res.json(thoughts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.route('/')
+  .get(getThoughts)
+  .post(createThought)
 
-// GET a single thought by ID
-router.get('/:thoughtId', async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    res.json(thought);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put()
+  .delete()
 
-// POST a new thought
-router.post('/', async (req, res) => {
-  try {
-    const thought = new Thought(req.body);
-    await thought.save();
-    res.status(201).json(thought);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// PUT update a thought by ID
-router.put('/:thoughtId', async (req, res) => {
-  try {
-    const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true });
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    res.json(thought);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// DELETE remove a thought by ID
-router.delete('/:thoughtId', async (req, res) => {
-  try {
-    const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    res.json({ message: 'Thought deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.route('/:thoughtId/reactions')
+  .post()
+  .delete()
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,64 +1,23 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser
+} = require('../../controllers/userController.js')
 
-// GET all users
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.route('/')
+  .get(getUsers)
+  .post(createUser)
 
-// GET a single user by ID
-router.get('/:userId', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser)
 
-// POST a new user
-router.post('/', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// PUT update a user by ID
-router.put('/:userId', async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// DELETE remove a user by ID
-router.delete('/:userId', async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json({ message: 'User deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.route('/:userId/friends/:friendId')
+  .post()
+  .delete()
 
 module.exports = router;
